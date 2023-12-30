ccd497f9a7625c0cd6b2d7342b925abcc69738fd
jhoanTrujillo <jhoantrujillo95@gmail.com>
2023-12-30 10:48:55 +0000

    add: sidenav from materialize

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,7 @@
+// Initialize sidenav 
+// This code was copied from the Google Materialize css framework
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
+

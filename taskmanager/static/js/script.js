29272d1bc4f445ed0545bce818f7af47b0df1e29
jhoanTrujillo <jhoantrujillo95@gmail.com>
2023-12-31 14:31:14 +0000

    add: ability to add task to to database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,5 +3,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelector('.sidenav');
     M.Sidenav.init(sidenav);
+
+     // datepicker initialization
+     let datepicker = document.querySelectorAll(".datepicker");
+     M.Datepicker.init(datepicker, {
+         format: "dd mmmm, yyyy",
+         i18n: {done: "Select"}
+     });
+ 
+     // select initialization
+     let selects = document.querySelectorAll("select");
+     M.FormSelect.init(selects);
   });
 
